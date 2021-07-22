12f6b6eebe07dc45ccfe0f583136d9b0a58745ef
David Silva <Davidslv@users.noreply.github.com>
2021-07-22 19:38:23 +0100

    Config and better naming

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Directory Service',
+  serviceName: 'Legal help directory',
 
   // Default port that prototype runs on
   port: '3000',
