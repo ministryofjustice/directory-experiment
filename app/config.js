1556d60751eea73526962696a2e2953ea4caa1bd
Ethan-McDowall <78203378+Ethan-McDowall@users.noreply.github.com>
2021-08-09 16:47:02 +0100

    new section for a2j

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'ELSA prototypes',
+  serviceName: '{serviceName}',
 
   // Default port that prototype runs on
   port: '3000',
