4824cdf06ae0582b4027463e64014bc2d512c6e1
Ethan-McDowall <78203378+Ethan-McDowall@users.noreply.github.com>
2021-08-13 11:21:21 +0100

    New content updates

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,14 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+//
+//router.post('/home-next-constructive', function (req, res) {
+//
+//   let nextConstruction = req.session.data['next-construction']
+//  
+//    if (nextConstruction === 'Under 18') {
+//     res.redirect('/prototype-2/cautions/caution-type')
+//    }  else {
+//      res.redirect('/prototype-2/start-page')
+//    }
+//  })
