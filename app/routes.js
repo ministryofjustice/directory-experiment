2e8374e09decb5a4ffb5a4a0b6cd913f8f17caba
Ethan-McDowall <78203378+Ethan-McDowall@users.noreply.github.com>
2021-09-01 16:42:03 +0100

    updated logic for the home page

--- app/routes.js
+++ app/routes.js
@@ -24,17 +24,39 @@ router.post('/option-one-route', function (req, res) {
     if (optionOneRoute === 'read the next option') {
      res.redirect('A2J/task-list-pattern/claim-unfair-dismissal')
     }  else {
-      res.redirect('A2J/task-list-pattern/home-v2')
+      res.redirect('A2J/task-list-pattern/constructive-dimissal-choice')
     }
   })
+
+router.post('/constructive-dismissal-route', function (req, res) {
+
+    let constructiveDismissal = req.session.data['constructive-dismissal']
+   
+     if (constructiveDismissal === 'no') {
+      res.redirect('A2J/task-list-pattern/constructive-dismissal-resign')
+     }  else {
+       res.redirect('A2J/task-list-pattern/home-v2')
+     }
+   }) 
 
 router.post('/option-two-route', function (req, res) {
 
    let optionTwoRoute = req.session.data['option-two']
   
     if (optionTwoRoute === 'return to the previous option') {
-     res.redirect('A2J/task-list-pattern/constructive-dismissal-resign')
+     res.redirect('A2J/task-list-pattern/claim-unfair-dismissal')
     }  else {
-      res.redirect('A2J/task-list-pattern/home-v2')
+      res.redirect('A2J/task-list-pattern/claim-unfair-dismissal-choice')
     }
   }) 
+
+  router.post('/unfair-dismissal-route', function (req, res) {
+
+    let unfairDismissal = req.session.data['unfair-dismissal']
+   
+     if (unfairDismissal === 'return to the previous option') {
+      res.redirect('A2J/task-list-pattern/claim-unfair-dismissal')
+     }  else {
+       res.redirect('A2J/task-list-pattern/home-v2')
+     }
+   }) 
